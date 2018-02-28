3a0b07dc6459d2055f934a4a887685eefa694b02
Ghada Messaoudi <ghada.messaoudi@seattlecentral.edu>
2018-02-28 12:19:22 -0700

    Adds People and Modifies Contact and About Pages

---
+++ BakeryFP/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BakeryFP.Models
+{
+    public class Message
+    {
+        public string MessageText { get; set; }
+        public Message() { }
+        public Message(string text)
+        {
+            MessageText = text;
+        }
+    }
+}
